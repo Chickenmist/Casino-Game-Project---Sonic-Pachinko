ceeb067babadb091cee7333ce7b4866a6a38222b
Wilson Pimlatt <wpimlatt@gmail.com>
2025-03-28 10:59:32 -0400

    Resized the window, started to set up the rectangle for the wall of the launcher

--- Casino Game Project - Sonic Pachinko/Game1.cs
+++ Casino Game Project - Sonic Pachinko/Game1.cs
@@ -4,10 +4,28 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Casino_Game_Project___Sonic_Pachinko
 {
+    //Wilson
+
+    //WaitScreen will allow bets to be accepted
+    enum Screen
+    {
+        WaitScreen,
+        PlayScreen
+    }
+
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        KeyboardState keyboardState;
+        MouseState mouseState;
+
+        Ball ball;
+        Texture2D ballTexture;
+        Rectangle ballRect;
+
+        Rectangle wallRect;
 
         public Game1()
         {
@@ -19,8 +37,15 @@ namespace Casino_Game_Project___Sonic_Pachinko
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 985;
+            _graphics.ApplyChanges();
+
+            wallRect = new Rectangle(45, 0, 0, 0);
 
             base.Initialize();
+
+            ball = new Ball(ballTexture, 0, 945);
         }
 
         protected override void LoadContent()
@@ -28,23 +53,34 @@ namespace Casino_Game_Project___Sonic_Pachinko
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            ballTexture = Content.Load<Texture2D>("sonicBall");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            keyboardState = Keyboard.GetState();
+            mouseState = Mouse.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+
+            Window.Title = mouseState.Position.ToString();
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.White);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            ball.Draw(_spriteBatch);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
