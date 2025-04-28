c886fb077fc24fb1a08e08bddb0e6d773869898f
Chickenmist <wpimlatt@gmail.com>
2025-04-28 15:52:47 -0400

    Worked on movement

--- Casino Game Project - Sonic Pachinko/Game1.cs
+++ Casino Game Project - Sonic Pachinko/Game1.cs
@@ -26,11 +26,21 @@ namespace Casino_Game_Project___Sonic_Pachinko
         KeyboardState keyboardState;
         MouseState mouseState;
 
+        Random random = new Random();
+        int dropSide;
+        int firstBumper;
+        bool dropStarted;
+
         Ball ball;
         Texture2D ballTexture;
         Rectangle ballRect;
         Vector2 ballSpeed;
+        Point ballPostionHolder;
         bool ballLaunched;
+
+        bool regBumperHit;
+        bool leftBumperHit;
+        bool rightBumperHit;
 
         float launchTime;
 
@@ -93,6 +103,7 @@ namespace Casino_Game_Project___Sonic_Pachinko
 
             currentScreen = Screen.WaitScreen;
             ballLaunched = false;
+            dropStarted = false;
             launchTime = 0;
 
             wallRect = new Rectangle(45, 185, 40, 800);
@@ -130,53 +141,58 @@ namespace Casino_Game_Project___Sonic_Pachinko
             ballSpeed = new Vector2(0, -10);
 
             bumpers = new List<Rectangle>();
+            
             //Row one
-            bumpers.Add(new Rectangle(150, 100, 51, 52));
-            bumpers.Add(new Rectangle(250, 100, 51, 52));
-            bumpers.Add(new Rectangle(350, 100, 51, 52));
-            bumpers.Add(new Rectangle(450, 100, 51, 52));
-            bumpers.Add(new Rectangle(550, 100, 51, 52));
-            bumpers.Add(new Rectangle(650, 100, 51, 52));
+            bumpers.Add(new Rectangle(150, 100, 52, 52));
+            bumpers.Add(new Rectangle(250, 100, 52, 52));
+            bumpers.Add(new Rectangle(350, 100, 52, 52));
+            bumpers.Add(new Rectangle(450, 100, 52, 52));
+            bumpers.Add(new Rectangle(550, 100, 52, 52));
+            bumpers.Add(new Rectangle(650, 100, 52, 52));
             //
 
             //Row two
-            bumpers.Add(new Rectangle(200, 250, 51, 52));
-            bumpers.Add(new Rectangle(300, 250, 51, 52));
-            bumpers.Add(new Rectangle(500, 250, 51, 52));
-            bumpers.Add(new Rectangle(600, 250, 51, 52));
+            bumpers.Add(new Rectangle(200, 250, 52, 52));
+            bumpers.Add(new Rectangle(300, 250, 52, 52));
+            bumpers.Add(new Rectangle(500, 250, 52, 52));
+            bumpers.Add(new Rectangle(600, 250, 52, 52));
             //
 
             //Row three
-            bumpers.Add(new Rectangle(150, 400, 51, 52));
-            bumpers.Add(new Rectangle(250, 400, 51, 52));
-            bumpers.Add(new Rectangle(350, 400, 51, 52));
-            bumpers.Add(new Rectangle(450, 400, 51, 52));
-            bumpers.Add(new Rectangle(550, 400, 51, 52));
-            bumpers.Add(new Rectangle(650, 400, 51, 52));
+            bumpers.Add(new Rectangle(150, 400, 52, 52));
+            bumpers.Add(new Rectangle(250, 400, 52, 52));
+            bumpers.Add(new Rectangle(350, 400, 52, 52));
+            bumpers.Add(new Rectangle(450, 400, 52, 52));
+            bumpers.Add(new Rectangle(550, 400, 52, 52));
+            bumpers.Add(new Rectangle(650, 400, 52, 52));
             //
 
             //Row four
-            bumpers.Add(new Rectangle(300, 550, 51, 52));
-            bumpers.Add(new Rectangle(400, 550, 51, 52));
-            bumpers.Add(new Rectangle(500, 550, 51, 52));
+            bumpers.Add(new Rectangle(300, 550, 52, 52));
+            bumpers.Add(new Rectangle(400, 550, 52, 52));
+            bumpers.Add(new Rectangle(500, 550, 52, 52));
             //
 
             //Row five
-            bumpers.Add(new Rectangle(150, 700, 51, 52));
-            bumpers.Add(new Rectangle(250, 700, 51, 52));
-            bumpers.Add(new Rectangle(350, 700, 51, 52));
-            bumpers.Add(new Rectangle(450, 700, 51, 52));
-            bumpers.Add(new Rectangle(550, 700, 51, 52));
-            bumpers.Add(new Rectangle(650, 700, 51, 52));
+            bumpers.Add(new Rectangle(150, 700, 52, 52));
+            bumpers.Add(new Rectangle(250, 700, 52, 52));
+            bumpers.Add(new Rectangle(350, 700, 52, 52));
+            bumpers.Add(new Rectangle(450, 700, 52, 52));
+            bumpers.Add(new Rectangle(550, 700, 52, 52));
+            bumpers.Add(new Rectangle(650, 700, 52, 52));
+            //
+            
+            //Left wall bumpers
+            leftWallBumpers = new List<Rectangle>();
+            leftWallBumpers.Add(new Rectangle(90, 250, 52, 52));
+            leftWallBumpers.Add(new Rectangle(90, 550, 52, 52));
             //
 
-            leftWallBumpers = new List<Rectangle>();
-            leftWallBumpers.Add(new Rectangle(90, 250, 51, 52));
-            leftWallBumpers.Add(new Rectangle(90, 550, 51, 52));
-
+            //Right wall bumpers
             rightWallBumpers = new List<Rectangle>();
-            rightWallBumpers.Add(new Rectangle(700, 250, 51, 52));
-            rightWallBumpers.Add(new Rectangle(700, 550, 51, 52));
+            rightWallBumpers.Add(new Rectangle(700, 250, 52, 52));
+            rightWallBumpers.Add(new Rectangle(700, 550, 52, 52));
+            //
 
             base.Initialize();
         }
@@ -225,26 +241,168 @@ namespace Casino_Game_Project___Sonic_Pachinko
                 {
                     if (keyboardState.IsKeyDown(Keys.Space)) // Initiate launch
                     {
-                        //launchTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-                        //if (launchTime < 3)
-                        //{
+                        firstBumper = new Random().Next(1, 7);
 
-                        //}
+                        ballSpeed = new Vector2(0, -10);
 
-                            ballLaunched = true;
+                        ballLaunched = true;
                     }
                 }
                 else
                 {
                     ballRect.Offset(ballSpeed);
-                    if (ballRect.Intersects(roofCurveLeftRect))
+                    if (ballRect.Intersects(roofCurveLeftMoveRectOne))
                     {
+                        ballSpeed = new Vector2(4.5f, -7.3f);
+                    }
+                    else if (ballRect.Intersects(roofCurveLeftMoveRectTwo))
+                    {
+                        ballSpeed = new Vector2(5.6f, -2.9f);
+                    }
+                    else if (ballRect.Intersects(roofCurveLeftMoveRectThree))
+                    {
+                        ballSpeed = new Vector2(10, 0);
+                    }
 
+                    if (!dropStarted)
+                    {
+                        if (firstBumper == 1)
+                        {
+                            if (ballRect.X >= 150 && ballRect.X <= 202)
+                            {
+                                ballSpeed = new Vector2(0, 5);
+                                dropSide = new Random().Next(1,3);
+                                dropStarted = true;
+                            }
+                        }
+                        else if (firstBumper == 2)
+                        {
+                            if (ballRect.X >= 250 && ballRect.X <= 302)
+                            {
+                                ballSpeed = new Vector2(0, 5);
+                                dropSide = new Random().Next(1, 3);
+                                dropStarted = true;
+                            }
+                        }
+                        else if (firstBumper == 3)
+                        {
+                            if (ballRect.X >= 350 && ballRect.X <= 402)
+                            {
+                                ballSpeed = new Vector2(0, 5);
+                                dropSide = new Random().Next(1, 3);
+                                dropStarted = true;
+                            }
+                        }
+                        else if (firstBumper == 4)
+                        {
+                            if (ballRect.X >= 450 && ballRect.X <= 502)
+                            {
+                                ballSpeed = new Vector2(0, 5);
+                                dropSide = new Random().Next(1, 3);
+                                dropStarted = true;
+                            }
+                        }
+                        else if (firstBumper == 5)
+                        {
+                            if (ballRect.X >= 550 && ballRect.X <= 602)
+                            {
+                                ballSpeed = new Vector2(0, 5);
+                                dropSide = new Random().Next(1, 3);
+                                dropStarted = true;
+                            }
+                        }
+                        else if (firstBumper == 6)
+                        {
+                            if (ballRect.X >= 650 && ballRect.X <= 702)
+                            {
+                                ballSpeed = new Vector2(0, 5);
+                                dropSide = new Random().Next(1, 3);
+                                dropStarted = true;
+                            }
+                        }
                     }
                     else
                     {
+                        foreach (Rectangle bumper in bumpers)
+                        {
+                            if (ballRect.Intersects(bumper))
+                            {
+                                regBumperHit = false;
+                                ballSpeed = new Vector2(0, 0);
+                                ballRect.Y = bumper.Top - ballRect.Height - 5;
+                                ballPostionHolder = ballRect.Location;
+                                dropSide = new Random().Next(1, 3);
+                                regBumperHit = true;
+                            }
+                            
+                            if (regBumperHit)
+                            {
+                                if (dropSide == 1) //Drop to the left
+                                {
+                                    ballSpeed = new Vector2(-5, 0);
 
+                                    if (ballRect.X >= ballPostionHolder.X - 100 && ballRect.X <= ballPostionHolder.X - 50)
+                                    {
+                                        ballSpeed = new Vector2(0, 5);
+                                    }
+                                }
+                                else if (dropSide == 2) //Drop to the right
+                                {
+                                    ballSpeed = new Vector2(5, 0);
+
+                                    if (ballRect.X <= ballPostionHolder.X + 100 && ballRect.X >= ballPostionHolder.X + 50)
+                                    {
+                                        ballSpeed = new Vector2(0, 5);
+                                    }
+                                }
+                            }
+                        }
+
+                        foreach (Rectangle leftbumper in leftWallBumpers)
+                        {
+                            if (ballRect.Intersects(leftbumper))
+                            {
+                                leftBumperHit = false;
+                                ballSpeed = new Vector2(0, 0);
+                                ballRect.Y = leftbumper.Top - ballRect.Height - 5;
+                                dropSide = 2;
+                                leftBumperHit = true;
+                            }
+
+                            if (leftBumperHit)
+                            {
+                                ballSpeed = new Vector2(5, 0);
+
+                                if (ballRect.X <= ballPostionHolder.X + 100 && ballRect.X >= ballPostionHolder.X + 50)
+                                {
+                                    ballSpeed = new Vector2(0, 5);
+                                }
+                            }
+                        }
+
+                        foreach (Rectangle rightbumper in rightWallBumpers)
+                        {
+                            if (ballRect.Intersects(rightbumper))
+                            {
+                                rightBumperHit = false;
+                                ballSpeed = new Vector2(0, 0);
+                                ballRect.Y = rightbumper.Top - ballRect.Height - 5;
+                                dropSide = 1;
+                                rightBumperHit = true;
+                            }
+
+                            if (rightBumperHit)
+                            {
+                                ballSpeed = new Vector2(-5, 0);
+
+                                if (ballRect.X >= ballPostionHolder.X - 100 && ballRect.X <= ballPostionHolder.X - 50)
+                                {
+                                    ballSpeed = new Vector2(0, 5);
+                                }
+                            }
+                        }
+                       
                     }
                 }
             }
